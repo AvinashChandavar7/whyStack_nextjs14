56707102769369e8a236e0851d95d1830ce652e1
avinashChandavar7 <avinashChnadavar7@gmail.com>
2023-12-27 17:34:48 +0530

    feat: basic Question Form (Ui layout)

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+import QuestionForm from '@/components/forms/QuestionForm';
+
+const Page = () => {
   return (
-    <div>Ask Question</div>
+    <div>
+
+      <h1 className='h1-bold text-dark100_light900'>
+        Ask a Question
+      </h1>
+
+      <div className='text-dark400_light800 mt-9'>
+        <QuestionForm />
+      </div>
+
+    </div>
   )
 }
 
-export default page
+export default Page;
---
+++ src/lib/validations.ts
@@ -0,0 +1,10 @@
+import * as z from "zod"
+
+
+export const QuestionsSchema = z.object({
+  title: z.string().min(5).max(130, { message: "title must be at least 5 characters.", }),
+  explanation: z.string().min(100),
+  tags: z.array(
+    z.string().min(1).max(15)
+  ).min(1).max(2),
+})
